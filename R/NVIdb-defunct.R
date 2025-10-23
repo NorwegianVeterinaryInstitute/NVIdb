5490e5eebe16e73eff58e003c4669b8d26f44658
Petter Hopp <petter.hopp@vetinst.no>
2025-10-23 08:43:44 +0200

    doc: rewrote NVIdb-defunct in accordance with NVIdb-deprecated Created help.

--- R/NVIdb-defunct.R
+++ R/NVIdb-defunct.R
@@ -1,16 +1,24 @@
-#' @title Defunct functions in NVIdb
-#' @Description The functions or variables listed here are no longer part of NVIdb.
-
+#' @title Defunct Functions in Package NVIdb
+#' @description These functions are no longer part of NVIdb.
+#'     When possible, alternative functions are mentioned. The original help
+#'     pages for defunct functions are available at \code{help("<function>-defunct")}.
+#' @details \code{set_PAT}, \code{get_PAT}, and \code{remove_PAT} was defunct from
+#'     v0.14.1 released 2025-10-## and had been deprecated from v0.11.0 released
+#'     2024-01-24. The functions were never taken into use. Functions from the
+#'     much better package \code{gitcreds} should be used instead.
+#'
+#' @param \dots (arguments)
+#' @return (results)
+#' @name NVIdb-defunct
+#' @keywords internal
+#'
+#' @author Petter Hopp Petter.Hopp@@vetinst.no
+#'
 #' @examples
 #' \dontrun{
-#' # Defunct from v0.14.1 (2025-10-##), deprected from v0.11.0 (2024-01-24)
-#' # set_PAT(...), get_PAT(...), and remove_PAT(...) were not or 
-#' #   rarely used. The old help pages can be found at 
-#' #   \code{help("set_PAT-defunct")}. \code{gitcreds} have corresponding and 
-#' #   better functions.
-#' set_PAT(...)
-#' get_PAT(...)
-#' remove_PAT(...)
+#' set_PAT(...) ### -- use gitcreds::gitcreds_set() instead.
+#' get_PAT(...) ### -- use gitcreds::gitcreds_get() instead.
+#' remove_PAT(...) ### -- use gitcreds::gitcreds_delete() instead.
 #' }
-#' @name NVIdb-defunct
-#' NULL
+#'
+NULL
