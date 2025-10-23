520779c7562540a8ad6380c1cc76a9b6acf7fc36
Petter Hopp <petter.hopp@vetinst.no>
2025-10-23 07:15:29 +0200

    feat: created NVIdb-defunct

---
+++ R/NVIdb-defunct.R
@@ -0,0 +1,16 @@
+#' @title Defunct functions in NVIdb
+#' @Description The functions or variables listed here are no longer part of NVIdb.
+
+#' @examples
+#' \dontrun{
+#' # Defunct from v0.14.1 (2025-10-##), deprected from v0.11.0 (2024-01-24)
+#' # set_PAT(...), get_PAT(...), and remove_PAT(...) were not or 
+#' #   rarely used. The old help pages can be found at 
+#' #   \code{help("set_PAT-defunct")}. \code{gitcreds} have corresponding and 
+#' #   better functions.
+#' set_PAT(...)
+#' get_PAT(...)
+#' remove_PAT(...)
+#' }
+#' @name NVIdb-defunct
+#' NULL
