60b3ae15e73069040acfe94bea53d53970c5a904
Petter Hopp <petter.hopp@vetinst.no>
2025-10-24 08:31:06 +0200

    test: created tests for defunct login_EOS, login_by_input_EOS, and login_by_credentials_EOS

--- tests/testthat/test_NVIdb_defunct.R
+++ tests/testthat/test_NVIdb_defunct.R
@@ -17,5 +17,17 @@ test_that("errors for defunct functions", {
                regexp = "'set_PAT', 'get_PAT', and 'remove_PAT' should be replaced by",
                fixed = TRUE)
 
+  expect_error(login_EOS(),
+               regexp = "'login_EOS' is replaced by 'login('EOS')' as the wrapper",
+               fixed = TRUE)
+
+  expect_error(login_by_input_EOS(),
+               regexp = "'login_by_input_EOS' is replaced by 'login_by_input('EOS')' as",
+               fixed = TRUE)
+
+  expect_error(login_by_credentials_EOS(),
+               regexp = "'login_by_credentials_EOS' is replaced by 'login_by_credentials('EOS')'",
+               fixed = TRUE)
+
   options(width = unlist(linewidth))
 })
