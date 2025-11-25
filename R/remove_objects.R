29ccbe9975de0f4bb4f0961cbf0d609e3c4ed4a8
Petter Hopp <petter.hopp@vetinst.no>
2025-11-25 06:58:11 +0100

    doc: created help for remove_objects

--- R/remove_objects.R
+++ R/remove_objects.R
@@ -1,28 +1,28 @@
 #' @title Remove objects if they exist in the environment
-#' @description Convinience wrapper around rm(). The function accepts a vector 
+#' @description Convenience wrapper around \code{rm}. The function accepts a vector
 #'     with named objects and checks if the objects exist before trying removing
 #'     them.
 #' @param objects [\code{character}]\cr
 #' Objects that should be removed.
-#' @param envir [\code{environment}]\cr 
-#' The environment from which the objects should be removed. Defaults to the 
-#'     function's parent environment, i.e. the environment from which the 
-#'     function was called. 
+#' @param envir [\code{environment}]\cr
+#' The environment from which the objects should be removed. Defaults to the
+#'     function's parent environment, i.e. the environment from which the
+#'     function was called.
 #' @return none, objects are removed from the environment.
-#' 
+#'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #' @export
 #' @examples
 #' # Reading from standard directory at NVI's network
 #' xx <- 12345
 #' remove_objects("xx")
-#' 
+#'
 #' xy <- 123
 #' xz <- 456
-#' remove_objects(c("xy", "xz", "zz")) 
-#' 
+#' remove_objects(c("xy", "xz", "zz"))
+#'
 remove_obects <- function(object, envir = parent.env(environment())) {
-  
+
   # ARGUMENT CHECKING ----
   # Object to store check-results
   checks <- checkmate::makeAssertCollection()
@@ -33,11 +33,11 @@ remove_obects <- function(object, envir = parent.env(environment())) {
   checkmate::assert_environment(envir, add = checks)
   # Report check-results
   checkmate::reportAssertions(checks)
-  
+
   # REMOVE OBJECTS FROM THE ENVIRONMENT ----
   # Identifies which objects that are in the environment
   objects_to_remove <- intersect(ls(name = envir), object)
-  
+
   # Removes identified objects from the environment
   rm(list = objects_to_remove, envir = envir)
 }
