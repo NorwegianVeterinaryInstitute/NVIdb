90b1d3ddab8f6d8bb58ed56e55f23555ede74122
Petter Hopp <petter.hopp@vetinst.no>
2025-11-25 08:10:30 +0100

    doc: minor changes in help for remove_objects

--- R/remove_objects.R
+++ R/remove_objects.R
@@ -2,7 +2,7 @@
 #' @description Convenience wrapper around \code{rm}. The function accepts a vector
 #'     with named objects and checks if the objects exist before trying removing
 #'     them.
-#' @param objects [\code{character}]\cr
+#' @param object [\code{character}]\cr
 #' Objects that should be removed.
 #' @param envir [\code{environment}]\cr
 #' The environment from which the objects should be removed. Defaults to the
@@ -13,13 +13,15 @@
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #' @export
 #' @examples
-#' # Reading from standard directory at NVI's network
+#' \dontrun{
+#' library(NVIdb)
 #' xx <- 12345
 #' remove_objects("xx")
 #'
 #' xy <- 123
 #' xz <- 456
 #' remove_objects(c("xy", "xz", "zz"))
+#' }
 #'
 remove_obects <- function(object, envir = parent.env(environment())) {
 
@@ -37,7 +39,6 @@ remove_obects <- function(object, envir = parent.env(environment())) {
   # REMOVE OBJECTS FROM THE ENVIRONMENT ----
   # Identifies which objects that are in the environment
   objects_to_remove <- intersect(ls(name = envir), object)
-
-  # Removes identified objects from the environment
+  # Removes existing objects from the environment
   rm(list = objects_to_remove, envir = envir)
 }
